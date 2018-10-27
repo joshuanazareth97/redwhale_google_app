42373d35e9c6086533161e83cf7ff1cd1e5fdfa6
Joshua <joshuanazareth97@gmail.com>
2018-10-27 22:12:09 +0530

    Fixed authentication views for urls

--- redwhale_google_app/urls.py
+++ redwhale_google_app/urls.py
@@ -3,12 +3,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-import .views as rw_views
+import google_auth.views as rw_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('social_django.urls', namespace="google")),
-    path('login/', auth_views.login, name='login'),
-    path('logout/', auth_views.logout, name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', rw_views.homepage, name='homepage')
 ]
