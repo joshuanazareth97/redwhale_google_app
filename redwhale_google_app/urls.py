a9cd09c7399dc16d493788013c72de3c2650a7de
Joshua <joshuanazareth97@gmail.com>
2018-10-27 21:33:56 +0530

    Changed google auth url

--- redwhale_google_app/urls.py
+++ redwhale_google_app/urls.py
@@ -7,7 +7,7 @@ import .views as rw_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('google_auth/', include('social_django.urls', namespace="google")),
+    path('auth/', include('social_django.urls', namespace="google")),
     path('login/', auth_views.login, name='login'),
     path('logout/', auth_views.logout, name='logout'),
     path('', rw_views.homepage, name='homepage')
