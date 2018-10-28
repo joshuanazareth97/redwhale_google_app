7981505d45ec0fdedb3bb05158ed621c779fac81
Joshua <joshuanazareth97@gmail.com>
2018-10-28 19:14:27 +0530

    Added SSL force settings if DEBUG turned off (default config)

--- redwhale_google_app/settings.py
+++ redwhale_google_app/settings.py
@@ -128,6 +128,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+#SSL settings
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
