9acb0d945e3c6ebf0a592bc350fb32eb231c7d15
Joshua <joshuanazareth97@gmail.com>
2018-10-28 18:06:14 +0530

    Added code to handle image upload to edit_profile view

--- google_auth/views.py
+++ google_auth/views.py
@@ -14,8 +14,16 @@ def edit_profile(request):
         bio = request.POST.get("bio")
         user_profile.bio = bio
         user_profile.save()
-        if request.FILES:
-            print("File")
+        image_file = request.FILES.get("display")
+        if image_file:
+            ext = image_file.name.split(".")[-1]
+            size = image_file.size/1024**2
+            print(ext,size)
+            if ext in ["png","jpg","bmp","gif", "jpeg"] and size < 50:
+                user_profile.display_picture.save("{}.{}".format(user_profile.user,ext),image_file)
+            else:
+                print("Error")
+                return render(request, "edit_details.html", {'profile': user_profile, 'error': "Please enter a valid image. [PNG, JPG, GIF, BMP]"})
         return redirect('/')
     else:
         return render(request, 'edit_details.html', {'profile':user_profile})
